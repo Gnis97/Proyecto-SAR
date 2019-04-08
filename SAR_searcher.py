a80889f32381925666869b9aa639f5deef160931
migarbo1 <49317526+migarbo1@users.noreply.github.com>
2019-04-08 12:43:51 +0200

    Update SAR_searcher.py

--- SAR_searcher.py
+++ SAR_searcher.py
@@ -25,6 +25,11 @@ def load_object(file_name):
         obj = pickle.load(fh)
     return obj
 
+def load_json(filename):
+    with open(filename) as fh:
+        obj = json.load(fh)
+    return obj
+
 def parsequerry(que):
     que.lower()
     que = que.split()
@@ -70,74 +75,62 @@ def consulta(in, q):
             operador = -1
     return res
 
-def load_json(filename):
-    with open(filename) as fh:
-        obj = json.load(fh)
-    return obj
-
 def intersection(p1,p2):
-    res = []
-    i = 0
-    j = 0
-    while i < len(p1) and  j < len(p2):
-        ep1 = p1[i]
-        ep2 = p2[j]
-        if ep1 == ep2:
-            res.append(ep1)
-            i+=1
-            j+=1
-        else:
-            if ep1 < ep2:
-                i+=1
-            else:
-                j+=1
-    return res
+	res = []
+	i = 0
+	j = 0
+	p1.sort()
+	p2.sort()
+	while i < len(p1) and  j < len(p2):
+		ep1 = p1[i]
+		ep2 = p2[j]
+		if ep1 == ep2:
+			res.append(ep1)
+			i+=1
+			j+=1
+		else:
+			if ep1 < ep2:
+				i+=1
+			else:
+				j+=1
+	return res
 
 def union(p1,p2):
-    res = []
-    i = 0
-    j = 0
-    while i < len(p1) and  j < len(p2):
-        ep1 = p1[i]
-        ep2 = p2[j]
-        if ep1 == ep2:
-            res.append(ep1)
-            i+=1
-            j+=1
-        else:
-            if ep1 < ep2:
-                res.append(ep1)
-                i+=1
-            else:
-                res.append(ep2)
-                j+=1
-    while i < len(p1):
-        ep1 = p1[i]
-        res.append(ep1)
-    while j < len(ep2):
-        ep2 = p2[j]
-        res.append(ep2)
-    return  res
+	res = []
+	i = 0
+	j = 0
+	p1.sort()
+	p2.sort()
+	while i < len(p1) and  j < len(p2):
+		ep1 = p1[i]
+		ep2 = p2[j]
+		if ep1 == ep2:
+			res.append(ep1)
+			i+=1
+			j+=1
+		else: 
+			if ep1 < ep2:
+				res.append(ep1)
+				i+=1
+			else:
+				res.append(ep2)
+				j+=1
+	while i < len(p1):
+		ep1 = p1[i]
+		res.append(ep1)
+	while j < len(ep2):
+		ep2 = p2[j]
+		res.append(ep2)
+	return  res
+	
+def diferencia(dic,p2):
+	res = []
+	for k,v in dic:
+		for e in v:
+			if e not in p2:
+				res.append(e)
+	return res
 
-def diferencia(p1,p2): "(p1 not p2)"
-    i = 0
-    j = 0
-    if len(p1) > len(p2):
-        res = p1
-    else:
-        res = p2
-    while i < len(p1) and  j < len(p2):
-        ep1 = p1[i]
-        ep2 = p2[j]
-        if ep1 == ep2:
-            res.remove(ep1)
-            j+=1
-        else:
-            if ep1 < ep2:
-                i+=1
-            else:
-                j+=1
-    return res
 
 if __name__ == "__main__":
     querry = None
